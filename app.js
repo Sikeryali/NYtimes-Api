c690553fe9df69ecaece63aee8bb0029acd4ed34
Asghar Usama - Komvux Kärnan <usama.asghar@skola.helsingborg.se>
2019-11-21 12:59:07 +0100

    first commit

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ $(() => {
         const searchTerm = $('#search-input').val();
         getRequest(searchTerm);
     });
-   function getRequest(input) {
+    function getRequest(input) {
         const url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
         const parameters = {
             q: input,
@@ -12,26 +12,24 @@ $(() => {
         };
         $.getJSON(url, parameters, (response) => {
             showResults(response.response.docs);
-         });
-         function showResults(articlesearch) {
-             console.log(articlesearch);
-           
-             
-         
-         $.each(articlesearch, (i, value)=> {
-            
-            $('#results').append(`<h1>${value.headline.main}</h1>`);
-            $('#results').append(`<h3>${value.snippet}</h3>`);
-            $('#results').append(`<p>${value.byline.original}</p>`);
-            $('#results').append(`<p>${value.lead_paragraph}</p>`);
-            $('#results').append(`<p>${value.news_desk}</p>`);
-            $('#results').append(`<p>${value.source}</p>`);
-            $('#results').append(`<p>${value.web_url}</p>`);
-            $('#results').append(`<p>${value.pub_date}</p>`);
         });
-        }
-   
-     }
-    
-});
        
+
+
+
+            $.each(articlesearch, (i, value) => {
+
+                $('#results').append(`<h1>${value.headline.main}</h1>`);
+                $('#results').append(`<h3>${value.snippet}</h3>`);
+                $('#results').append(`<p>${value.byline.original}</p>`);
+                $('#results').append(`<p>${value.lead_paragraph}</p>`);
+                $('#results').append(`<p>${value.news_desk}</p>`);
+                $('#results').append(`<p>${value.source}</p>`);
+                $('#results').append(`<p>${value.web_url}</p>`);
+                $('#results').append(`<p>${value.pub_date}</p>`);
+            });
+        
+
+    }
+
+});
